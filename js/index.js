ec36117a4274280da781c0c0db0104968daaa86c
ToMaTo1206 <114519489+ToMaTo1206@users.noreply.github.com>
2025-10-27 13:23:59 +0100

    changement couleurs et du css et  du js

--- js/index.js
+++ js/index.js
@@ -1,10 +1,20 @@
 const btn = document.getElementById("btn-mode");
+const p_mode = document.getElementById("p-mode");
 
-btn.addEventListener("click", () => {
+function textContent(){
     if (document.body.classList.contains("jour")) {
+        p_mode.textContent = "Nuit";
         document.body.classList.remove("jour");
         document.body.classList.toggle("nuit");
     } else if (document.body.classList.contains("nuit")) {
+        p_mode.textContent = "Jour";
         document.body.classList.remove("nuit");
         document.body.classList.toggle("jour");
-}});
+};
+}
+
+
+btn.addEventListener("click", () => {
+    textContent();
+
+});
